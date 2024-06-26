fbc87a09a531dbbf5a9d121802a71837fbf6483e
DyRoss5 <dyamondross5@gmail.com>
2024-06-26 15:44:47 -0500

    <Candidate Testing>

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,7 +13,7 @@ let candidateAnswer = "";
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? ",
 "True or false: 5 kilometer == 5000 meters? ",
-"(5+3)/2*10 =? ",
+"(5 + 3)/2 * 10 = ? ",
 "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
 "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride","true","40","Trajectory", "3"];
@@ -40,10 +40,9 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-//for (i=0; i < candidateAnswers.length; i++);
-  candidateAnswers[i] = [""];
+
 let output = `The candidate's answers are ${candidateAnswers}. The correct answers are ${correctAnswers}.`;
-  if (candidateAnswers[i] === correctAnswers[i]); {
+if (candidateAnswers === correctAnswers); {
    
   } console.log(output);
   
